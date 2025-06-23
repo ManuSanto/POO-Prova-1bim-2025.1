689219618c5ffc730f99a9bda90dbc91a7fe7596
DudisTrindade <eduarda.trindade@escolar.ifrn.edu.br>
2025-06-23 14:25:09 -0300

    Prt 3

--- Prova 1bim 2025.1/Assets/Cod Meu/Elementalista.cs
+++ Prova 1bim 2025.1/Assets/Cod Meu/Elementalista.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MovimentoJogador : MonoBehaviour
+public class Elementalista : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
